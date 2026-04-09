0392e8ce38fef142a8d4416ba9bea1df0a412c40
Juan Alonso <juanalonso966@gmail.com>
2026-04-09 17:43:37 -0600

    fix: add new URL in server.js

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://elotes-41o8wrv5p-jmaa1.vercel.app",
+      "https://elotes-app.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
   }),
