2278c82ea96a6f029826645c27a3c3f23a7bd1ce
Juan Alonso <juanalonso966@gmail.com>
2026-04-17 16:48:00 -0600

    add routes in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,35 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import connectDB from "./src/config/db.js";
+import auth from "./src/routes/auth.js";
+import produtos from "./src/routes/productos.js";
+import ventas from "./src/routes/ventas.js";
+import gastos from "./src/routes/gastos.js";
+import reportes from "./src/routes/reportes.js";
+import authMiddleware from "./src/middleware/auth.js";
 
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("OK 🚀");
+  res.send("API funcionando 🚀");
 });
 
-app.post("/test", (req, res) => {
-  res.json({ ok: true });
-});
+app.use("/api/auth", auth);
+app.use("/api/productos", authMiddleware, produtos);
+app.use("/api/ventas", authMiddleware, ventas);
+app.use("/api/gastos", authMiddleware, gastos);
+app.use("/api/reportes", authMiddleware, reportes);
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server corriendo en puerto", PORT);
-});
+const startServer = async () => {
+  await connectDB();
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server Listener On Port ${PORT}`);
+  });
+};
+startServer();
